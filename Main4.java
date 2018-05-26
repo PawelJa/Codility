1c416511896dc72b225f9b51e490360bf3d3a19a
PawelJa <janecki.paw@gmail.com>
2018-05-26 14:08:00 +0100

    FrogImp

---
+++ Main4.java
@@ -0,0 +1,15 @@
+public class Main4 {
+
+    public static void main (String [] args) {
+
+        int x = 10;
+        int y = 85;
+        int d = 30;
+
+        if ((y-x) % d == 0 ) {
+            System.out.println((y-x) / d);
+        } else {
+            System.out.println((y-x) / d +1 );
+        }
+    }
+}
